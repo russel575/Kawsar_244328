ffa3b51fa42b0920ea05cc2444878344ef5015e8
russel575 <mdrussel575@gmail.com>
2019-05-28 10:43:10 +0600

    update

---
+++ FormValidation/FormValidation/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FormValidation
+{
+    partial class FormValidation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.User = new System.Windows.Forms.Label();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(221, 59);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(158, 20);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Location = new System.Drawing.Point(126, 65);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(29, 13);
+            this.User.TabIndex = 1;
+            this.User.Text = "User";
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(113, 229);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(266, 154);
+            this.showRichTextBox.TabIndex = 2;
+            this.showRichTextBox.Text = "";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(304, 200);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(221, 85);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(158, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(221, 116);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(158, 20);
+            this.ageTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Age";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(414, 62);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(0, 13);
+            this.userLabel.TabIndex = 4;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(417, 119);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(0, 13);
+            this.ageLabel.TabIndex = 5;
+            // 
+            // FormValidation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Name = "FormValidation";
+            this.Text = "Form Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label ageLabel;
+    }
+}
+
---
+++ FormValidation/FormValidation/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormValidation
+{
+    public partial class FormValidation : Form
+    {
+
+        List<string> names = new List<string>();
+        List<string> users = new List<string>();
+        List<int> ages = new List<int>();
+        
+
+        public FormValidation()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+            
+            try
+            {
+                string user;
+                string name = nameTextBox.Text;
+                int age;
+
+                userLabel.Text = "";
+                if (!String.IsNullOrEmpty(userTextBox.Text))
+                {
+                    user = user = userTextBox.Text; 
+                   
+                }
+                else
+                {
+                    userLabel.Text = "Field Must Not be Empty";
+                    return;
+                }
+
+                ageLabel.Text = "";
+                    if (!String.IsNullOrEmpty(ageTextBox.Text))
+                {
+                    age = Convert.ToInt32(ageTextBox.Text);
+                    
+                }
+                else
+                {
+                    ageLabel.Text = "Field Must Not be Empty";
+                    return;
+                }
+
+                
+                users.Add(user);
+                names.Add(name);
+                ages.Add(age);
+
+
+                showRichTextBox.Text = display();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            
+
+
+        }
+
+        private string display()
+        {
+            string message = "";
+            message = "SL\tUser\tName\tAge\n";
+
+            int index = 0;
+
+            foreach (string user in users)
+                message = message + (index+1) + "\t" + user + "\t" + names[index] + "\t" + ages[index] + "\n";
+
+            return message;
+
+               }
+    }
+}
